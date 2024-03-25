fb10ba5b5132a7080685a78c46cc91777dcf326c
Sudarshan Sanjay Bhosale <bhosalesudarshan10@gmail.com>
2024-03-25 15:02:59 +0530

    background added

--- app/src/main/java/com/example/moengage_assignment_android/activity/HeadingBasedOnSourceActivity.kt
+++ app/src/main/java/com/example/moengage_assignment_android/activity/HeadingBasedOnSourceActivity.kt
@@ -2,6 +2,7 @@ package com.example.moengage_assignment_android.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,10 @@ import com.example.moengage_assignment_android.NewsArticleApiResponse
 import com.example.moengage_assignment_android.R
 import com.example.moengage_assignment_android.adapter.NewsItemAdapter
 import org.json.JSONArray
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class HeadingBasedOnSourceActivity : AppCompatActivity() {
 
@@ -29,7 +34,29 @@ class HeadingBasedOnSourceActivity : AppCompatActivity() {
 
         // Setup RecyclerView
         rvNewsTitle.layoutManager = LinearLayoutManager(this)
-        rvNewsTitle.adapter = NewsItemAdapter(articlesList)
+//        rvNewsTitle.adapter = NewsItemAdapter(articlesList)
+
+        val latestArticles = articlesList.sortedByDescending { parseDateTime(it.publishedAt) }
+        showArticles(latestArticles)
+        val tvLatest=findViewById<TextView>(R.id.tvLatest)
+        val tvEarliest=findViewById<TextView>(R.id.tvEarliest)
+
+        tvLatest.setOnClickListener {
+            val latestArticles = articlesList.sortedByDescending { parseDateTime(it.publishedAt) }
+            showArticles(latestArticles)
+            tvLatest.setBackgroundResource(R.drawable.rounded_background)
+            tvEarliest.setBackgroundResource(R.drawable.plaiin_backgroubd)
+
+        }
+
+        tvEarliest.setOnClickListener {
+            val earliestArticles = articlesList.sortedBy { parseDateTime(it.publishedAt) }
+            showArticles(earliestArticles)
+            tvEarliest.setBackgroundResource(R.drawable.rounded_background)
+            tvLatest.setBackgroundResource(R.drawable.plaiin_backgroubd)
+
+
+        }
 
 
 
@@ -61,5 +88,25 @@ class HeadingBasedOnSourceActivity : AppCompatActivity() {
             Log.e("ParsingError", "Error parsing JSON: ${e.message}")
         }
         return articlesList
+    }
+    private fun parseDateTime(dateTimeString: String?): Date {
+        try {
+            if (dateTimeString.isNullOrEmpty()) {
+                // Return a default date if the input string is null or empty
+                return Date(0)
+            }
+            val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+            return formatter.parse(dateTimeString) ?: Date(0)
+        } catch (e: ParseException) {
+            // Handle parsing exception here
+            Log.e("DateTimeParsing", "Error parsing date: ${e.message}")
+            return Date(0) // Return a default date in case of parsing error
+        }
+    }
+
+
+
+    private fun showArticles(articlesList: List<NewsArticleApiResponse.Article>) {
+        rvNewsTitle.adapter = NewsItemAdapter(articlesList)
     }
 }
